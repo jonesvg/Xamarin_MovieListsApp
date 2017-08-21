945abc5be9cb954117f51d1bdf27edb060f33111
jones.goncalves <jones.goncalves@andritz.com>
2017-08-21 13:51:17 -0300

    Changed the language of the consults in TMDb to get the current culture of the mobile. If us-EN the app will gets the results in us-EN, if in pt-BR will gets the results in pt-BR

--- MoviesListProject/MoviesListProject.Android/MainActivity.cs
+++ MoviesListProject/MoviesListProject.Android/MainActivity.cs
@@ -15,7 +15,7 @@ namespace MoviesListProject.Droid
             base.OnCreate(bundle);
 
             global::Xamarin.Forms.Forms.Init(this, bundle);
-
+            
             LoadApplication(new App());
         }
     }
