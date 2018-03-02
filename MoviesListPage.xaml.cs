01f79defb4fd1df8b3e2c7e0d5b0bfecc11ec89e
jonesvg <jonesvg@gmail.com>
2018-03-02 08:44:39 -0300

    Commit the new version of code
    I have worked in the command and viewmodels code, I have removed some extra code and worked with more async and Tasks

---
+++ MoviesListPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using MoviesListProject.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TmdbServiceConnector;
+using TmdbServiceConnector.Models;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MoviesListProject.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MoviesListPage : ContentPage
+    {
+        private MoviesListViewModel viewModel;
+
+        public MoviesListPage(DataServiceConnector _service, int _listType)
+        {
+            InitializeComponent();
+            BindingContext = viewModel = new MoviesListViewModel(_service, this.Navigation, _listType);
+        }
+        
+    }
+}
